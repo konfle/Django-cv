cf23ca317ff580619e8faaa7a8b24ffcd88e48f5
konfle <konrad.flejter@gmail.com>
2020-03-23 22:36:46 +0100

    remove / from url and change about section

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -4,11 +4,11 @@ from django.urls import path,include
 app_name = 'portfolio'
 urlpatterns = [
     path('', views.index, name='index'),
-    path('about/',views.about, name="about" ),
-    path('experience/',views.experience, name="experience" ),
-    path('education/',views.education, name="education" ),
-    path('skills/',views.skills, name="skills" ),
-    path('languages/',views.languages, name="languages" ),
+    path('about',views.about, name="about" ),
+    path('experience',views.experience, name="experience" ),
+    path('education',views.education, name="education" ),
+    path('skills',views.skills, name="skills" ),
+    path('languages',views.languages, name="languages" ),
     path('certification',views.certification, name="certification" ),
     ]
 
